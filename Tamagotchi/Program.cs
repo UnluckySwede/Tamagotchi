335b04beefcf3fc97220ca672c26d387927b5640
UnluckySwede <gadler.michael@elev.ga.ntig.se>
2022-09-21 13:11:23 +0200

    Final

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,35 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Greetings! What would you like your character to be called?");
+System.Console.CursorVisible = true;
+Console.WriteLine("Greetings! What would you like your Tamagotchi to be called?");
 
 Tamagotchi character = new Tamagotchi();
 character.name = Console.ReadLine();
 
-character.Hi();
+System.Console.WriteLine($"You've named your Tamagotchi to {character.name}, make sure you take care of it!");
 
 while (character.GetAlive())
 {
-    System.Console.WriteLine("What would you like to do today? Teach or eat?");
+    Console.Clear();
+    character.PrintStats();
+    System.Console.WriteLine($"What would you like to do with {character.name} today?");
+    System.Console.WriteLine("1) Teach");
+    System.Console.WriteLine("2) Feed");
+    System.Console.WriteLine($"3) Say hello to {character.name}");
+    System.Console.WriteLine("4) Nothing");
 
     string activity = Console.ReadLine().ToLower();
 
-    if (activity == "teach")
+    if (activity == "1")
     {
-        character.Teach();
+        System.Console.WriteLine("What word");
+        string word = Console.ReadLine();
+        character.Teach(word);
     }
-    if (activity == "eat")
+    if (activity == "2")
     {
         character.Feed();
     }
+    if (activity == "3")
+    {
+        character.Hi();
+    }
 
     character.Tick();
-    character.PrintStats();
 
 }
 
-
+System.Console.WriteLine($"Oh no {character.name} died. Bruh...");
 
 Console.ReadLine();
-
-
-
--- Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi.cs
@@ -5,37 +5,63 @@ public class Tamagotchi
 
     private int hunger = 0;
     private int boredom = 0;
-    private List<string> words = new List<string>() { "arabic", "english", "french" };
+    private List<string> words = new List<string>() { };
     private bool isAlive = true;
     private Random gen = new Random();
 
     public string name = "tamagotchi";
 
-
     public void Feed()
     {
-        System.Console.WriteLine("You eat some bread.");
-        hunger = 0;
+        System.Console.WriteLine($"{name} ate some bread.");
+        if (hunger >= 3)
+        {
+            hunger -= 3;
+        }
+        else
+        {
+            hunger = 0;
+        }
     }
     public void Hi()
     {
-        System.Console.WriteLine($"Greetings {name}, hope you enjoy your stay!");
+        if (words.Count == 0)
+        {
+            Console.WriteLine("I don't know any words yet...");
+        }
+        else
+        {
+            int wordNum = gen.Next(words.Count);
+            System.Console.WriteLine($"[{name}] says: {words[wordNum]}.");
+            ReduceBoredom();
+        }
     }
-    public void Teach()
+    public void Teach(string word)
     {
-        System.Console.WriteLine($"You learned how to read {words}.");
+        System.Console.WriteLine($"[{name}] learned how to say {word}.");
+        words.Add(word);
         ReduceBoredom();
     }
     public void Tick()
     {
         hunger += gen.Next(1, 2);
         boredom += gen.Next(1, 2);
+
+        if (hunger >= 10)
+        {
+            isAlive = false;
+        }
+        if (boredom >= 10)
+        {
+            isAlive = false;
+        }
     }
     public void PrintStats()
     {
+        System.Console.WriteLine($"Name : {name}");
         System.Console.WriteLine($"Boredom = {boredom}");
         System.Console.WriteLine($"Hunger = {hunger}");
-        System.Console.WriteLine($"Alive = {isAlive}");
+        System.Console.WriteLine($"Knowledge = {words.Count}");
     }
     public bool GetAlive()
     {
@@ -44,7 +70,13 @@ public class Tamagotchi
     }
     private void ReduceBoredom()
     {
-        boredom = 0;
+        if (boredom >= 3)
+        {
+            boredom -= 3;
+        }
+        else
+        {
+            boredom = 0;
+        }
     }
-
 }
